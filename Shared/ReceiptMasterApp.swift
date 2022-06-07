c7fbd071b199c2f291b4e59d627c1065a11ff5f9
Changhao Song <changhao.song@mail.utoronto.ca>
2022-06-06 20:34:01 -0400

    Initial Commit

---
+++ Shared/ReceiptMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptMasterApp.swift
+//  Shared
+//
+//  Created by Changhao Song on 2022-06-06.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
